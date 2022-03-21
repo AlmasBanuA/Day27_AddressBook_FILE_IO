0b6b1bdf4b7b57409e13f39508a5f5089e9c733d
[AlmasBanuA2011] <[almasbanu3304@gmail.com]>
2022-03-21 20:27:11 +0530

    Creating contacts in Address Book

---
+++ src/com/day27/AddressBook.java
@@ -0,0 +1,55 @@
+package com.day27;
+
+/**
+ * UC1:- Ability to create a Contacts in Address Book with first and last names,
+ * address, city, state, zip, phone number and email...
+ * 
+ * @author user -Almas
+ *
+ */
+public class AddressBook {
+
+	private class Contact {
+		String firstName, lastNmae, address, city, state, emailId;
+		int zipCode;
+		long mobileNumber;
+	}
+
+	/**
+	 * created method printContact for creating contacts in AddressBook
+	 */
+	public void printContact() {
+		Contact person = new Contact();
+		person.firstName = "Almas";
+		person.lastNmae = "Banu";
+		person.address = "Gandhinagar";
+		person.city = "Davanagere";
+		person.state = "Karnataka";
+		person.zipCode = 577601;
+		person.mobileNumber = 9090909090L;
+		person.emailId = "almasbanu20111998@gmail.com";
+		System.out.println("Contact Details");
+		System.out.println("Name          : " + person.firstName + " " + person.lastNmae + "\n" + "Address       : "
+				+ person.address + "\n" + "City          : " + person.city + "\n" + "State         : " + person.state
+				+ "\n" + "ZipCode       : " + person.zipCode + "\n" + "MobileNumber  : " + person.mobileNumber + "\n"
+				+ "EmailId       : " + person.emailId + "\n");
+	}
+
+	/**
+	 * Main method for manipulation Of Collections
+	 * 
+	 * @param args - Default Java param (Not used)
+	 */
+	public static void main(String[] args) {
+		/**
+		 * created instance of AddressBook class
+		 */
+		AddressBook addressBook = new AddressBook();
+		/**
+		 * Calling method printContact for displaying contacts
+		 */
+		addressBook.printContact();
+
+	}
+
+}
